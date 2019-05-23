e0fa79d7417ae867f733a23ef3af8ce569104445
Paul Martin <communty.hivemind@gmail.com>
2019-05-23 09:51:32 +0100

    Remove unused code
    Removed commented out code and unused bits and pieces

--- services/api.mjs
+++ services/api.mjs
@@ -1,9 +1,8 @@
 import Utils from './helper-methods.js';
-let xhr = new XMLHttpRequest();
-let xhr2 = new XMLHttpRequest();
 
 let api = {
   getData: function() {
+    let xhr = new XMLHttpRequest();
     let url = `https://quiet-everglades-27917.herokuapp.com/api/data`;
 
     xhr.open('GET', url, true);
@@ -15,8 +14,6 @@ let api = {
         let apiData = JSON.parse(xhr.response);
         let response = Utils.createWeatherObjects(apiData);
 
-        console.log(apiData);
-
         Utils.outputWeatherObjectsTable(response.list);
         Utils.updateWeatherPointsDashboard(response.list);
         Utils.createWeatherCharts(response);
@@ -24,27 +21,23 @@ let api = {
     };
   },
 
-  getDarkSkyData: function(callback) {
-    console.log('hello'); 
-
+  getDarkSkyData: function() {
+    let xhr = new XMLHttpRequest();
     let corsUnblock = 'https://cors-anywhere.herokuapp.com/'
     let url = 'https://api.darksky.net/forecast/a22ed4ddcc0df574eed15d7d838dde0a/51.5173735,-0.0731095';
-    xhr2.open('GET', corsUnblock + url, true);
-    xhr2.send();
+    xhr.open('GET', corsUnblock + url, true);
+    xhr.send();
     
-    xhr2.onload = function() {
-      let darkskyData = JSON.parse(xhr2.response)
+    xhr.onload = function() {
+      let darkskyData = JSON.parse(xhr.response)
       let response = Utils.createExternalWeatherObjects(darkskyData);
-      // let twentyfourhours = response.list.slice(0, 25)
-      // console.log(response.getPrecipIntensityData());
       Utils.createDarkskyWeatherCharts(response);
-
-      callback();
     }
     
   },
 
   getDataByDates: function (initial, final) {
+    let xhr = new XMLHttpRequest();
     let initDate = initial.toISOString()
     let finalDate = final.toISOString()
     let url = `https://quiet-everglades-27917.herokuapp.com/api/data`;
@@ -61,24 +54,6 @@ let api = {
  
         Utils.outputWeatherObjectsTable(response.list);
         Utils.updateWeatherCharts(response);
-      }
-    };
-  },
-  
-  // No need for this at the moment
-  getWeatherChart: function() {
-    let url = `https://quiet-everglades-27917.herokuapp.com/api/data`;
-
-    xhr.open('GET', url, true);
-    xhr.send();
-    xhr.onload = function() {
-      if (xhr.status != 200) {
-        alert(`Error ${xhr.status}: ${xhr.statusText}`);
-      } else {
-        let apiData = JSON.parse(xhr.response);
-        let response = Utils.createWeatherObjects(apiData);
-        // Utils.createWeatherChart(apiData);
-        Utils.createWeatherChart(response);
       }
     };
   }
